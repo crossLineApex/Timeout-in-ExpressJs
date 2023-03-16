59e106ba6c45836d4ae773330d4a2a9ee94e3fa8
crossLineApex <56408469+crossLineApex@users.noreply.github.com>
2023-03-16 12:57:20 +0530

    adding comments

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.use((req,res,next) => {
   res.setTimeout(5000,() => {
     return res.status(408).json({messgae: "timeout response"});
-  });
+  }); // adding setTimeout to response
   next();
 })
 
@@ -14,11 +14,11 @@ return new Promise((resolve,reject) => {
     resolve({id: 1});
   },10000);
 });
-};
+}; // an async operation which takes 10 sec to return a promise
 
 app.get("/", async(req,res) => {
   const data = await asyncWork();
-  if(res.writableEnded || res.writableFinished || res.headersSent){
+  if(res.writableEnded || res.writableFinished || res.headersSent){ // to ensure that we are not sending more than one response on same route
     console.log("response timed out");
   }else{
     res.json(data);
